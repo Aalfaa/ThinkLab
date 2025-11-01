be5cef0d77e6acdea0c25fe083d086517986c60d
Aalfaa <alvianfarhat5@gmail.com>
2025-11-01 09:43:24 +0000

    Nov 1, 2025, 4:43 PM

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  AOS.init({
-    duration: 1000,
-    once: true
+  AOS.init({ duration: 1000, once: true });
+
+  const nav = document.querySelector("nav");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) nav.classList.add("bg-[#2D3142]", "shadow-md");
+    else nav.classList.remove("bg-[#2D3142]", "shadow-md");
   });
 });
